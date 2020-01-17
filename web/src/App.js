72d9299f7d34f323671575e2f674be0877acbc1b
“marcelotakayama” <marcelo.takayama00@gmail.com>
2020-01-17 15:05:43 -0300

    Adição dos arquivos da interface web, porém sem excluir os arquivos que nem no vídeo

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header';
+
+function App() {
+  return (
+    <>
+      <Header title="Título 1" />
+      <Header title="Título 2" />
+      <Header title="Título 3" />
+    </>
+  );
+}
+ 
+export default App;
