da4eff7623b337ee004f4c2d0b41e6118ef1dc98
“marcelotakayama” <marcelo.takayama00@gmail.com>
2020-01-17 17:00:21 -0300

    Início da estilização da aplicação web 46:41

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import Header from './Header';
 
 function App() {
+
   return (
-    <>
-      <Header title="Título 1" />
-      <Header title="Título 2" />
-      <Header title="Título 3" />
-    </>
+    <div id = "app">
+      <aside>
+
+      </aside>
+      <main></main>
+    </div>
   );
 }
  
