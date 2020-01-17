682cfe17590462345f5a8283bd063a512bdb4a82
“marcelotakayama” <marcelo.takayama00@gmail.com>
2020-01-17 17:16:19 -0300

    Estou indo embora 49:02

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import './global.css';
+import './App.css';
+
 
 function App() {
 
   return (
     <div id = "app">
       <aside>
-
+        <strong>Cadastrar</strong>
+        <form>
+          <label></label>
+        </form>
       </aside>
       <main></main>
     </div>
