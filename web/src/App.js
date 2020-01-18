9f8baf8b0ef05767c1003bea50400ab3e78b5804
“marcelotakayama” <marcelo.takayama00@gmail.com>
2020-01-18 03:39:25 -0300

    Estrutura da aplicação pronta, tudo funcionando 1:09:21

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 
 function App() {
@@ -12,10 +14,82 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <label></label>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+
         </form>
       </aside>
-      <main></main>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47531526?s=460&v=4" alt="Marcelo Takayama" />
+              <div className="user-info">
+                <strong>Marcelo Takayama</strong>
+                <span>HTML, CSS, JS, NodeJS</span>
+              </div>            
+            </header>
+            <p>Estudante de Ciência da Computação, com interesses em desenvolvimento Front-end, Back-end e Mobile</p>
+            <a href="https://github.com/marcelotakayama">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47531526?s=460&v=4" alt="Marcelo Takayama" />
+              <div className="user-info">
+                <strong>Marcelo Takayama</strong>
+                <span>HTML, CSS, JS, NodeJS</span>
+              </div>            
+            </header>
+            <p>Estudante de Ciência da Computação, com interesses em desenvolvimento Front-end, Back-end e Mobile</p>
+            <a href="https://github.com/marcelotakayama">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47531526?s=460&v=4" alt="Marcelo Takayama" />
+              <div className="user-info">
+                <strong>Marcelo Takayama</strong>
+                <span>HTML, CSS, JS, NodeJS</span>
+              </div>            
+            </header>
+            <p>Estudante de Ciência da Computação, com interesses em desenvolvimento Front-end, Back-end e Mobile</p>
+            <a href="https://github.com/marcelotakayama">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/47531526?s=460&v=4" alt="Marcelo Takayama" />
+              <div className="user-info">
+                <strong>Marcelo Takayama</strong>
+                <span>HTML, CSS, JS, NodeJS</span>
+              </div>            
+            </header>
+            <p>Estudante de Ciência da Computação, com interesses em desenvolvimento Front-end, Back-end e Mobile</p>
+            <a href="https://github.com/marcelotakayama">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
